a784656438dbd2ed6c67b2d9f5e69545d8a59199
swarnava75 <ghoshswarnava49@gmail.com>
2025-08-30 02:27:24 +0530

    order received

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-function addToCart(item, price) {
-  const message = document.getElementById("cart-message");
-  message.innerText = `${item} added to cart for ₹${price}`;
-  message.style.opacity = "1";
+document.addEventListener("DOMContentLoaded", () => {
+  const orderForm = document.getElementById("orderForm");
+  const orderMessage = document.getElementById("orderMessage");
 
-  setTimeout(() => {
-    message.style.opacity = "0";
-  }, 3000);
-}
+  if (orderForm) {
+    orderForm.addEventListener("submit", function (e) {
+      e.preventDefault(); 
+
+     
+      orderMessage.innerText = "🎉 We have received your order successfully!";
+      orderMessage.style.display = "block";
+      orderMessage.style.color = "#00ffcc";
+      orderMessage.style.fontSize = "1.3rem";
+      orderMessage.style.fontWeight = "bold";
+      orderMessage.style.marginTop = "20px";
+      orderMessage.style.textShadow = "0 0 10px rgba(0,255,200,0.8)";
+
+      
+      orderForm.reset();
+
+      
+      setTimeout(() => {
+        orderMessage.style.display = "none";
+      }, 5000);
+    });
+  }
+});
+
